e7f4efa13e7a25f88791b850acaa50dee66cef34
estiakahamed01 <tasin220116@diit.edu.bd>
2026-05-20 16:34:56 +0600

    Added Bee1011

---
+++ bee_solve1011.kt
@@ -0,0 +1,8 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val r = readLine()!!.toDouble()
+    val pi = 3.14159
+    val valumn = (4.0 / 3.0)* (pi * r * r *r)
+    println("VOLUME = %.3f".format(valumn))	
+}
