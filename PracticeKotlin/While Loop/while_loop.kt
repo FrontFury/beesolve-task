3c0e0f17ef301173f7faa78258217ea489573e58
estiakahamed01 <tasin220116@diit.edu.bd>
2026-05-23 19:10:37 +0600

    Added Odd Number

--- PracticeKotlin/While Loop/while_loop.kt
+++ PracticeKotlin/While Loop/while_loop.kt
@@ -1,9 +1,19 @@
 //Print 1-20 using While Loop 
 
+// fun main(){
+//     var i = 1
+//     while (i<=20){
+//         println(i)
+//         i++
+//     }
+// }
+
+//Odd Number 
 fun main(){
+    println("Odd Number 1-20")
     var i = 1
     while (i<=20){
         println(i)
-        i++
+        i+=2
     }
 }
