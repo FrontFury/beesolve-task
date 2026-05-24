072ac1f69ec690fe221ca7caf484a0574abbc269
estiakahamed01 <tasin220116@diit.edu.bd>
2026-05-24 21:08:12 +0600

    Added Even Number : 1-20 using for in loop

--- PracticeKotlin/Loop - For In/forInLoop.kt
+++ PracticeKotlin/Loop - For In/forInLoop.kt
@@ -5,8 +5,15 @@
 // }
 
 // Odd Number using for in loop
+// fun main(){
+//     for(i in 1..20 step 2){
+//         println(i)
+//     }
+// }
+
+//Even Number : 1-20
 fun main(){
-    for(i in 1..20 step 2){
+    for(i in 2..20 step 2){
         println(i)
     }
 }
