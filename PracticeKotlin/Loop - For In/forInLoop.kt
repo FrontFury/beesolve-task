9c1456cfa1074fc164db4125cf421bdb96770d3c
estiakahamed01 <tasin220116@diit.edu.bd>
2026-05-24 21:06:48 +0600

    Added find Odd number using for in loop

--- PracticeKotlin/Loop - For In/forInLoop.kt
+++ PracticeKotlin/Loop - For In/forInLoop.kt
@@ -1,5 +1,12 @@
+// fun main(){
+//     for(i in 1..20){
+//         println(i)
+//     }
+// }
+
+// Odd Number using for in loop
 fun main(){
-    for(i in 1..20){
+    for(i in 1..20 step 2){
         println(i)
     }
 }
