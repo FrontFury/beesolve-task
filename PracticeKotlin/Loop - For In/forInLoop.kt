a965a5ab7e970ed2edd366c4d89aac9adfb7f19b
estiakahamed01 <tasin220116@diit.edu.bd>
2026-05-24 21:11:34 +0600

    Added Reversly Odd & Even Check

--- PracticeKotlin/Loop - For In/forInLoop.kt
+++ PracticeKotlin/Loop - For In/forInLoop.kt
@@ -19,8 +19,15 @@
 // }
 
 //Reverse Print - Downto
+// fun main(){
+//     for(i in 20 downTo 1){
+//         println(i)
+//     }
+// }
+
+//Reversly Odd & Even Check
 fun main(){
     for(i in 20 downTo 1){
-        println(i)
+        println("$i is ${if (i % 2 == 0) "Even" else "Odd"}")
     }
 }
